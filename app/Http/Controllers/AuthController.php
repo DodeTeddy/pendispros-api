8c1758603926442d0dacbbfa490221f44d35c86d
Dode Teddy <dodeteddy@Teds-MacBook-Air.local>
2023-03-30 09:41:34 +0800

    slay

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,38 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function login(Request $request){
-        $this->validate($request, [
-            'login'    => 'required',
-            'password' => 'required',
-        ]);
+        // $this->validate($request, [
+        //     'login'    => 'required',
+        //     'password' => 'required',
+        // ]);
     
-        $login_type = filter_var($request->input('login'), FILTER_VALIDATE_EMAIL ) 
-            ? 'email' 
-            : 'username';
+        // $login_type = filter_var($request->input('login'), FILTER_VALIDATE_EMAIL ) 
+        //     ? 'email' 
+        //     : 'username';
     
-        $request->merge([
-            $login_type => $request->input('login')
-        ]);
+        // $request->merge([
+        //     $login_type => $request->input('login')
+        // ]);
        
-        if (Auth::attempt($request->only($login_type, 'password'))) {
-            $user = Auth::user();
-            $token =  $user->createToken('my-token')->plainTextToken;
-            return response()->json([
-                'message' => 'Login Success!',
-                'token' => $token
-            ],200);
-        }else{
+        // if (Auth::attempt($request->only($login_type, 'password'))) {
+        //     $user = Auth::user();
+        //     $token =  $user->createToken('my-token')->plainTextToken;
+        //     return response()->json([
+        //         'message' => 'Login Success!',
+        //         'token' => $token
+        //     ],200);
+        // }else{
+        //     return response()->json([
+        //         'message' => 'Login Failed!',
+        //         'token' => null
+        //     ],422);
+        // }
+        if (! Auth::attempt($request->only('email', 'password'))) {
             return response()->json([
                 'message' => 'Login Failed!',
                 'token' => null
-            ],422);
+            ], 401);
         }
+
+        $user = User::where('email', $request->email)->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Login success',
+            'token' => $token
+        ], 200);
     }
     
     public function profile(){
